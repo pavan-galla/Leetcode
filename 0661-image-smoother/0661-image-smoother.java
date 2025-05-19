bcfddab3c31df4758b764ea799ffbb15a3d60748
pavan-galla <pavankumar1326.sr@gmail.com>
2025-05-19 14:00:24 +0530

    Time: 214 ms (6.59%), Space: 46 MB (25.88%) - LeetHub

---
+++ 0661-image-smoother/0661-image-smoother.java
@@ -0,0 +1,33 @@
+public class Solution {
+    public int[][] imageSmoother(int[][] img) {
+        int m = img.length;
+        int n = img[0].length;
+        int[][] result = new int[m][n];
+
+        // Directions array for all 8 neighbors + the cell itself
+        int[] dx = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
+        int[] dy = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int sum = 0;
+                int count = 0;
+
+                // Loop through all 9 positions
+                for (int k = 0; k < 9; k++) {
+                    int x = i + dx[k];
+                    int y = j + dy[k];
+
+                    if (x >= 0 && x < m && y >= 0 && y < n) {
+                        sum += img[x][y];
+                        count++;
+                    }
+                }
+
+                result[i][j] = sum / count; // floor division
+            }
+        }
+
+        return result;
+    }
+}
